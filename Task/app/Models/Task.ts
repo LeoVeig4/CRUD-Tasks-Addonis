53a2b0d89561398eea3c7f4e140700dcc80d150d
Leonardo da Veiga <leoveiga2002@gmail.com>
2022-10-20 16:57:17 -0300

    Initial commit

---
+++ Task/app/Models/Task.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, BelongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class Task extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public nome: string
+
+  @column()
+  public info: string
+
+  @column()
+  public done: boolean
+
+  @column()
+  public user_id: number
+
+  @belongsTo( () => User)
+  public user: BelongsTo<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
